99cbe342239611daa194faaf474200ea3e130345
repl.it user <replituser@example.com>
2021-03-08 05:02:04 +0000

    Simpe if example

---
+++ main.py
@@ -0,0 +1,12 @@
+#Check whether first number is bigger than the second number
+#Assume first_number and second_number are existing variables
+print("Please input first number")
+first_number=input() 
+print("First number is", first_number)
+print("Please input second number")
+second_number=input()
+print("Second number is", second_number)
+
+if(first_number>second_number):
+  print("First number is bigger!")
+print("Done!")
